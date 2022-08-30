ece1d0a959618ea4b9245b1a6ee369e9b1095f04
Lucy Gelderloos <68394281+lucy-gelderloos@users.noreply.github.com>
2022-08-29 21:12:03 -0700

    Revert "Hello world"

--- src/test/java/com/gelderloos/songr/SongrApplicationTests.java
+++ src/test/java/com/gelderloos/songr/SongrApplicationTests.java
@@ -2,29 +2,12 @@ package com.gelderloos.songr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
-
 
 @SpringBootTest
 class SongrApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test void testCreateAlbum() {
-		Album testAlbum = new Album("How It Ends", "DeVotchKa",14,3000,"https://upload.wikimedia.org/wikipedia/en/6/6c/DeVotchKa_-_How_it_Ends.jpg");
-
-		assertEquals("How It Ends",testAlbum.getTitle());
-	}
-
-	@Test void testGetNSet() {
-		Album testAlbum = new Album("How It Ends", "DeVotchKa",14,3000,"https://upload.wikimedia.org/wikipedia/en/6/6c/DeVotchKa_-_How_it_Ends.jpg");
-
-		assertEquals("DeVotchKa",testAlbum.getArtist());
-
-		testAlbum.setArtist("Devotchka");
-		assertEquals("Devotchka",testAlbum.getArtist());
 	}
 
 }

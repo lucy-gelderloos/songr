26ca5e6e2a0b61373fa2df782678e647d8f1d231
Lucy Gelderloos <lucygel@gmail.com>
2022-08-31 17:04:29 -0700

    Convert primitives to wrappers

--- src/main/java/com/gelderloos/songr/repositories/AlbumRepository.java
+++ src/main/java/com/gelderloos/songr/repositories/AlbumRepository.java
@@ -4,9 +4,7 @@ import com.gelderloos.songr.models.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
-        // The reason we are using an interface, is so we can create CUSTOM CRUD queries
 
-        // DARK MAGIC that we made happen with a specific function name
         public Album findByArtist(String artist);
         public Album findByTitle(String title);
     }
